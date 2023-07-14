a302f04897198229973fe1981d9298452e790f33
Aaron Stannard <aaron@petabridge.com>
2023-07-14 14:21:11 -0500

    remove benchmarks and other junk

--- src/Akka.Remote.Chunking/Class1.cs
+++ src/Akka.Remote.Chunking/Class1.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using Akka.Actor;
+using Akka.Remote.Transport;
 
 namespace Akka.Remote.Chunking
 {
-    public class Class1
+    public sealed class ChunkingTransportAdapterProvider : ITransportAdapterProvider
     {
+        public Transport.Transport Create(Transport.Transport wrappedTransport, ExtendedActorSystem system)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
