d2d8b9dfaeeaae1a95f87769d25e6fc13bd694bf
Aaron Stannard <aaron@petabridge.com>
2023-07-26 11:48:29 -0500

    defining protobuf format

--- src/Akka.Cluster.Chunking/DeliveryProtocol.cs
+++ src/Akka.Cluster.Chunking/DeliveryProtocol.cs
@@ -17,27 +17,39 @@ public interface IDeliveryProtocol
 }
 
 /// <summary>
+/// Marker interface for messages going over the wire in the chunked delivery system.
+/// </summary>
+public interface INetworkedDeliveryProtocol : IDeliveryProtocol{ }
+
+/// <summary>
 /// Input to chunked delivery system.
 /// </summary>
 /// <param name="Payload">The real, underlying message.</param>
 /// <param name="Recipient">The recipient (must not be null)</param>
 /// <param name="ReplyTo">The sender, as far as the remote actor knows (optional)</param>
-public sealed record ChunkedDelivery(object Payload, IActorRef Recipient, IActorRef? ReplyTo = null) : IDeliveryProtocol;
+public sealed record ChunkedDelivery(object Payload, IActorRef Recipient, IActorRef? ReplyTo = null) : INetworkedDeliveryProtocol;
 
 /// <summary>
 /// Registers a consumer node with a producer node.
 /// </summary>
 /// <param name="ConsumerController">A reference pointing to the ConsumerController.</param>
-public sealed record RegisterConsumer(IActorRef ConsumerController) : IDeliveryProtocol;
+public sealed record RegisterConsumer(IActorRef ConsumerController) :INetworkedDeliveryProtocol;
 
 /// <summary>
 /// User to confirm that both nodes are now registered with one another.
 /// </summary>
-public sealed class RegisterAck : IDeliveryProtocol
+public sealed class RegisterAck : INetworkedDeliveryProtocol
 {
     // make singleton
     public static readonly RegisterAck Instance = new();
     private RegisterAck(){}
+}
+
+public sealed class RegisterNack : INetworkedDeliveryProtocol
+{
+    // make singleton
+    public static readonly RegisterNack Instance = new();
+    private RegisterNack(){}
 }
 
 /// <summary>
