bd1af60ce4e008df3da0393f9f3d5f4e4429d1e1
Aaron Stannard <aaron@petabridge.com>
2023-07-26 15:13:44 -0500

    adding sample

---
+++ src/samples/Node/AkkaConfig.cs
@@ -0,0 +1,13 @@
+﻿using Akka.Cluster;
+using Akka.Remote.Hosting;
+
+namespace Shared;
+
+public class AkkaConfig
+{
+    public required string ActorSystemName { get; set; }
+    
+    public required RemoteOptions RemoteSettings { get; set; }
+    
+    public required ClusterSettings ClusterSettings { get; set; }
+}
---
+++ src/samples/Node/Program.cs
@@ -0,0 +1,33 @@
+﻿using Akka.Actor;
+using Akka.Actor.Dsl;
+using Akka.Cluster.Chunking.Configuration;
+using Akka.Cluster.Hosting;
+using Akka.Cluster.Sharding;
+using Akka.Hosting;
+using Akka.Util;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using SeedNode;
+using Shared;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false)
+            .AddEnvironmentVariables();
+    })
+    .ConfigureServices((hostContext, services) =>
+    {
+        var akkaConfig = hostContext.Configuration.GetSection("AkkaConfiguration").Get<AkkaConfig>();
+        services.AddAkka(akkaConfig.ActorSystemName, builder =>
+        {
+            builder
+                .AddChunkingManager() // MUST LAUNCH CHUNKING PLUGIN AT STARTUP ON ALL NODES
+                .WithActors((system, registry, arg3) =>
+            {
+                system.ActorOf(Props.Create(() => new PrinterActor()), "printer");
+                system.ActorOf(Props.Create(() => new SenderActor()), "sender");
+            });
+        });
+    })
+    .Build();
