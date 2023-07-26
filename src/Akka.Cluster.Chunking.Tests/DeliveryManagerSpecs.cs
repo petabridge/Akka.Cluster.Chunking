831a2fa1a7bd14fc4be77bd80caf3d36578915b6
Aaron Stannard <aaron@petabridge.com>
2023-07-26 13:24:29 -0500

    added a node restart spec

--- src/Akka.Cluster.Chunking.Tests/DeliveryManagerSpecs.cs
+++ src/Akka.Cluster.Chunking.Tests/DeliveryManagerSpecs.cs
@@ -102,4 +102,64 @@ public class DeliveryManagerSpecs : TestKit.Xunit2.TestKit
         probe.Reply("ok3");
         await probe3.ExpectMsgAsync("ok3");
     }
+
+    [Fact(DisplayName = "Should deliver messages to node after a restart")]
+    public async Task ShouldDeliverMessagesAfterNodeRestart()
+    {
+        // arrange
+        await EnsureClusterFormed();
+
+        var dm1 = CreateDeliveryManager(Sys);
+        var dm2 = CreateDeliveryManager(Sys2);
+        var dm3 = CreateDeliveryManager(Sys3);
+
+        var probe = CreateTestProbe(Sys);
+        var probe2 = CreateTestProbe(Sys2);
+        var probe3 = CreateTestProbe(Sys3);
+        
+        // have Sys message Sys2
+        var msg = new ChunkedDelivery("hello", probe2.Ref, probe.Ref);
+        dm1.Tell(msg);
+        await probe2.ExpectMsgAsync("hello");
+        probe2.Reply("ok");
+        await probe.ExpectMsgAsync("ok");
+        
+        // shut down Sys2
+        await WithinAsync(TimeSpan.FromSeconds(10),async () =>
+        {
+            var cluster1 = Akka.Cluster.Cluster.Get(Sys);
+            Shutdown(Sys2);
+            await AwaitConditionAsync(() => cluster1.State.Members.Count == 2);
+        });
+
+        // have Sys message Sys3 (validate that we didn't break connections to healthy nodes)
+        var msg2 = new ChunkedDelivery("hello2", probe3.Ref, probe.Ref);
+        dm1.Tell(msg2);
+        await probe3.ExpectMsgAsync("hello2");
+        probe3.Reply("ok2");
+        await probe.ExpectMsgAsync("ok2");
+        
+        // restart Sys2
+        var newSys2 = ActorSystem.Create(Sys.Name, Sys.Settings.Config);
+        var dm2New = CreateDeliveryManager(newSys2);
+        var probe2New = CreateTestProbe(newSys2);
+        
+        // rejoin cluster
+        await WithinAsync(TimeSpan.FromSeconds(10),async () =>
+        {
+            var cluster1 = Akka.Cluster.Cluster.Get(Sys);
+            var cluster2 = Akka.Cluster.Cluster.Get(newSys2);
+            var cluster3 = Akka.Cluster.Cluster.Get(Sys3);
+            cluster2.JoinSeedNodes(
+                new[] { cluster1.SelfAddress, cluster2.SelfAddress, cluster3.SelfAddress });
+            await AwaitConditionAsync(() => cluster1.State.Members.Count == 3);
+        });
+        
+        // message Sys2 from Sys3
+        var msg3 = new ChunkedDelivery("hello3", probe2New.Ref, probe3.Ref);
+        dm3.Tell(msg3);
+        await probe2New.ExpectMsgAsync("hello3");
+        probe2New.Reply("ok3");
+        await probe3.ExpectMsgAsync("ok3");
+    }
 }
